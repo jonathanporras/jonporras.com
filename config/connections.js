0ec7dafbbd1bee676d68fc61b544e79463a1f734
jonathanporras <porrasjon@gmail.com>
2015-07-19 18:11:28 -0400

    another connections update

--- config/connections.js
+++ config/connections.js
@@ -90,7 +90,7 @@ module.exports.connections = {
   ***************************************************************************/
   productionMongoHqDb: {
     adapter: 'sails-mongo',
-    url: process.env.MONGOHQ_URL
+    url: process.env.MONGOLAB_URI
   }
 
   // 'production': {
