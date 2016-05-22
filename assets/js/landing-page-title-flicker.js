d89534dd0859ae8c234de2bb4d2232e0c48254c4
jonathanporras <porrasjon@gmail.com>
2016-05-22 19:43:04 -0400

    chore: turn h2 into scroll-down link

--- assets/js/landing-page-title-flicker.js
+++ assets/js/landing-page-title-flicker.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 	$('.page-wrapper').fadeIn(1000);
   $('.splash-page').css('height', window.innerHeight);
+
+
+  $(".title-text").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#documents").offset().top
+    }, 2000);
+});
 });
 
 function randomNum() {
